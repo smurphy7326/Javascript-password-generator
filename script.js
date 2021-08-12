b4b5ff6e539875485f4dd5db165ac9f477b2bf1a
Your Name <you@example.com>
2021-08-12 14:11:11 -0400

    password length

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 // Assignment code here 
+let generateBtn = document.querySelector("#generate");
+console.log(generateBtn)
 
+// Array reference
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+const lc = "abcdefghijklmnopqrstuvwxyz";
 
+const uc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+const spec = "~!@#$%^&*()_+{}|'<>?,./`[]";
+
+const num = "0123456789";
+
+letallChar = "";
+
+//Prompt questions and answer section
+function generatePassword() {
+  console.log("Button was clicked")
+  let pwdLength = prompt(
+    "Between 8 and 128 characters, how long would you like your password to be?"
+  );
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
